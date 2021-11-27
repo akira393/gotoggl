3a6a6bee3a8507df264e8bbc4349b9b51419c4bf
katsuyama akiyoshi <katsuyama@gitlab.com>
2021-11-27 17:35:09 +0900

    :tada: (tag) tagのサブコマンド作成
    Closes #5
    
    :tada: (tag) tagサブコマンドの作成
    
    :pencil2: (tag)

---
+++ cmd/tag.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/akira393/togglclient"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	rootCmd.AddCommand(tagCmd())
+}
+
+func tagCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tag",
+		Short: "Manage tag resources",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+	cmd.AddCommand(
+		tagGetCmd(),
+	)
+	return cmd
+}
+
+func tagGetCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get",
+		Short: "Print tag",
+		Run:   runtagGetCmd,
+	}
+	return cmd
+}
+
+func runtagGetCmd(cmd *cobra.Command, args []string) {
+	token := viper.GetString("TOGGL_API_TOKEN")
+	if token == "" {
+		fmt.Println("Unable to locate credentials. You can configure credentials by running 'gotoggl configure'.")
+		return
+	}
+	session := togglclient.OpenSession(token)
+
+	account, err := session.GetAccount()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("index", ",", "id", ",", "name")
+	for i, v := range account.Data.Tags {
+		fmt.Println(i, ",", v.ID, ",", v.Name)
+	}
+
+	return
+}
